9db494f76d999b52b59f611d04117ce76bb4d405
Ayush Aggarwal <ayush.agarwal7689@gmail.com>
2016-03-26 18:18:44 +0530

    final commit

--- firstfollow.java
+++ firstfollow.java
@@ -6,24 +6,62 @@ public class firstfollow
 	public static List<String>first1=new ArrayList<String>();
 	public static void first(char a)
 	{
-		for(int i=0;i<prod.size();i++)
-			if(prod.get(i).get(0).toString().charAt(0)==a)
-				for(int j=1;j<prod.get(i).size();j++)
-					if(!Character.isUpperCase(prod.get(i).get(j).toString().charAt(0)))
-						first1.add(Character.toString(prod.get(i).get(j).toString().charAt(0)));
-					else if(prod.get(i).get(j).equals("ϵ"))
-						first1.add("ϵ");
-					else
-					{
-						first(prod.get(i).get(j).toString().charAt(0));
-						for(int k=1;k<prod.get(i).get(j).toString().length();k++)
-							if(first1.contains("ϵ"))
+		if(!Character.isUpperCase(a))
+				first1.add(Character.toString(a));
+		else
+		{
+			for(int i=0;i<prod.size();i++)
+				if(prod.get(i).get(0).toString().charAt(0)==a)
+					for(int j=1;j<prod.get(i).size();j++)
+						if(!Character.isUpperCase(prod.get(i).get(j).toString().charAt(0)))
+							first1.add(Character.toString(prod.get(i).get(j).toString().charAt(0)));
+						else if(prod.get(i).get(j).equals("ϵ"))
+							first1.add("ϵ");
+						else
+						{
+							if(prod.get(i).get(j).toString().charAt(0)!=a)
 							{
-								first1.remove("ϵ");
-								first(prod.get(i).get(j).toString().charAt(k));
+							first(prod.get(i).get(j).toString().charAt(0));
+							for(int k=1;k<prod.get(i).get(j).toString().length();k++)
+								if(first1.contains("ϵ"))
+								{
+									first1.remove("ϵ");
+									first(prod.get(i).get(j).toString().charAt(k));
+								}
 							}
-					}
+						}
+		}
 	}
+	public static void follow(char a)
+	{
+		if(a==prod.get(0).get(0).toString().charAt(0))
+			first1.add("$");
+		for(int j=0;j<prod.size();j++)
+			{
+				for(int k=1;k<prod.get(j).size();k++)
+					if(prod.get(j).get(k).toString().indexOf(a)!=-1)
+					{
+						for(int l=1;l<=prod.get(j).get(k).toString().length();l++)
+						{
+							int in=prod.get(j).get(k).toString().indexOf(a)+l;
+							if(in<prod.get(j).get(k).toString().length())
+							{
+								first(prod.get(j).get(k).toString().charAt(in));
+								if(first1.contains("ϵ"))
+								{
+									first1.remove("ϵ");
+									continue;
+								}
+								else
+									break;
+							}
+							else if(in>=prod.get(j).get(k).toString().length())
+								if(a!=prod.get(j).get(0).toString().charAt(0))
+									follow(prod.get(j).get(0).toString().charAt(0));
+						}
+					}
+			}
+		}
 	public static void main(String args[]) throws IOException
 	{	
 		int nop,f=0;
@@ -45,6 +83,15 @@ public class firstfollow
 		{
 			first(prod.get(i).get(0).toString().charAt(0));
 			System.out.print("FIRST OF "+prod.get(i).get(0).toString().charAt(0)+" is:- ");
+			Set<String>fir=new HashSet<String>(first1);
+			System.out.println(fir.toString());
+			first1.clear();
+		}
+		for(int i=0;i<nop;i++)
+		{
+			char a=prod.get(i).get(0).toString().charAt(0);
+			follow(a);
+			System.out.print("FOLLOW OF "+a+" is:- ");
 			Set<String>fir=new HashSet<String>(first1);
 			System.out.println(fir.toString());
 			first1.clear();
